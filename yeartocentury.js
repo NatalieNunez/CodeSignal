6b648a36ab35f6247aee80e4392068ca046abb6b
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-02-18 12:30:56 -0800

    Completed Year to Centruy Problem.

---
+++ yeartocentury.js
@@ -0,0 +1,9 @@
+function centuryFromYear(year) {
+    return Math.ceil(year/100);
+    }
+
+    /*
+    ### INSTRUCTIONS ###
+    Given a year, return the century it is in. The first century spans from the year 1 up to and 
+    including the year 100, the second - from the year 101 up to and including the year 200, etc.
+    */
