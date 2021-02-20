6714588ffb8dd8496cbcf28e8977f8b4bd93b33b
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-02-19 23:55:03 -0800

    Completed check palindrome

---
+++ checkpalindrome.js
@@ -0,0 +1,13 @@
+function checkPalindrome(inputString) {
+    let inputArr = inputString.split('');
+    let reverseArr = [];
+    for (let i = inputArr.length-1; i > 0; i--) {
+        reverseArr.push(inputArr[i]);
+    }
+    let reverseStr = reverseArr.join("");
+    if (inputString === reverseStr) {
+        return true;
+    } else {
+        return false;
+    }
+    }
