81a506a10ac166a3c414b7849648002cf96680a4
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-07-20 06:56:12 +0000

    completed all-longest-strings challenge

---
+++ all-longest-strings.ts
@@ -0,0 +1,11 @@
+function allLongestStrings(inputArray: string[]): string[] {
+    let longestStringsArray: string[] = []
+    let lengths = inputArray.map(string => string.length)
+    const longestLength = Math.max(...lengths)
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i].length === longestLength) {
+            longestStringsArray.push(inputArray[i])
+        }
+    }
+    return longestStringsArray
+}
