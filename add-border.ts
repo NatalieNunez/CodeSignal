5348f1480de08a257dfc083141290cc9b93768bb
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-08-09 04:16:23 +0000

    renamed variables, commented out pseudocode

--- add-border.ts
+++ add-border.ts
@@ -1,15 +1,13 @@
-function addBorder(picture: string[]) {
-    // find length (length of first index) and add 2
-    // find height (length of array) and add 2
-    // for side borders, we need to unshift and push an * into each index of the array
-    // for top borders, we need to unshift and psuh a value with length + 2 *
-
+function addBorder(picture: string[]): string[] {
     for (let i = 0; i < picture.length; i++) {
         picture[i] = `*${picture[i]}*`
     }
-    const topBorderLength = picture[0].length
-    const topBorder = '*'.repeat(topBorderLength)
-    picture.unshift(topBorder)
-    picture.push(topBorder)
+
+    const topAndBottomLength = picture[0].length
+    const horizontalBorder = '*'.repeat(topAndBottomLength)
+
+    picture.unshift(horizontalBorder)
+    picture.push(horizontalBorder)
+
     return picture
 }
