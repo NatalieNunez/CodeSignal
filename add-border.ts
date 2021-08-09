c5f7c65637f00d0b2201a0196dd9eaaf4243b6d8
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-08-09 03:38:53 +0000

    added asterick to beginning and end of each index

---
+++ add-border.ts
@@ -0,0 +1,11 @@
+function addBorder(picture: string[]) {
+    // find length (length of first index) and add 2
+    // find height (length of array) and add 2
+    // for side borders, we need to unshift and push an * into each index of the array
+    // for top borders, we need to unshift and psuh a value with length + 2 *
+
+    for (let i = 0; i < picture.length; i++) {
+        picture[i] = `*${picture[i]}*`
+    }
+
+}
