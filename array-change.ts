ec4f16d152b7b351bab5921d51fb1675a7148a38
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-08-11 02:46:02 +0000

    pseudocode for array-change

---
+++ array-change.ts
@@ -0,0 +1,9 @@
+function arrayChange(inputArray: number[]): number {
+    // initialize variable at 0 to keep count of number of moves
+    // loop through array
+        // if value at current index is greater than value at index + 1
+        // -> then increment value at index + 1 by 1 AND increment count variable
+
+    // dont actually return 0, just trying to please typescript for now
+    return 0
+}
